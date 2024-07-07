79edb6bf022b67d8a13d04c4403ca064a31ed883
akTARDIGRADE13 <itmtha@gmail.com>
2024-07-08 04:09:08 +0900

    feat: github pagesのための設定

--- visualizer/vite.config.ts
+++ visualizer/vite.config.ts
@@ -1,8 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+const cherryPickedKeys = [
+  'SOME_KEY_IN_YOUR_ENV_FILE',
+  'SOME_OTHER_KEY_IN_YOUR_ENV_FILE',
+];
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv: { [key: string]: string } = {};
+  cherryPickedKeys.forEach((key) => (processEnv[key] = env[key]));
+
+  return {
+    define: {
+      'process.env': processEnv,
+    },
+    plugins: [react(), tsconfigPaths()],
+    base: '/OptimizationTheoryReport/',
+    resolve: {
+      alias: {
+        '/src/main.tsx': '/src/OptimizationTheoryReport/main.tsx',
+      },
+    },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  };
 });
