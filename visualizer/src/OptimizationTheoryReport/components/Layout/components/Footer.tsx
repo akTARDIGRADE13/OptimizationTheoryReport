2f0dba631cf98752cefdb0e978397fbe1f6dcb15
akTARDIGRADE13 <itmtha@gmail.com>
2024-07-09 03:55:52 +0900

    feat: Layoutの設定

---
+++ visualizer/src/OptimizationTheoryReport/components/Layout/components/Footer.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { type FC } from 'react';
+import styles from './Footer.module.css';
+
+const Footer: FC = () => {
+  return <footer className={styles.footer}></footer>;
+};
+
+export default Footer;
